4cdf5e09e29854f83f408996809accf992f2fc0d
Pratiksha Andhare <pratikshaandhare93@gmail.com>
2024-04-27 10:51:31 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+# app.py
+
+from flask import Flask
+from flask_cors import CORS
+from routes import media_playback, search
+
+app = Flask(__name__)
+CORS(app)
+
+# Register blueprints
+app.register_blueprint(media_playback)
+app.register_blueprint(search)
+
+if __name__ == '__main__':
+    app.run(debug=True)
