7e7e082f8dc31c40cdb4308eb83397b01695f72c
Pratiksha Andhare <pratikshaandhare93@gmail.com>
2024-06-19 15:20:40 +0530

    app updated

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask
 from flask_cors import CORS
-from routes import media_playback, search
+from routes import media_playback, search, highlight
 
 app = Flask(__name__)
 CORS(app)
@@ -10,6 +10,8 @@ CORS(app)
 # Register blueprints
 app.register_blueprint(media_playback)
 app.register_blueprint(search)
+app.register_blueprint(highlight)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
