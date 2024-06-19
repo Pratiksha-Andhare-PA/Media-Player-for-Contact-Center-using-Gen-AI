029195d0c9e985aad00bdfdd92812c3a32557d88
Pratiksha Andhare <pratikshaandhare93@gmail.com>
2024-06-19 15:22:01 +0530

    code updated

--- routes.py
+++ routes.py
@@ -1,13 +1,13 @@
 # routes.py
 
 from flask import Blueprint, jsonify, send_file, request, render_template
-from utils import generate_chapters_from_transcript, search_api
+from utils import generate_chapters_from_transcript, search_api, generate_highlights,  generate_insights_from_transcript
 import json
 
 media_playback = Blueprint('media_playback', __name__)
 search = Blueprint('search', __name__)
+highlight = Blueprint('highlight', __name__)
 
-# Load video data from data.json
 with open('data.json', 'r') as json_file:
     video_data = json.load(json_file)
 
@@ -22,9 +22,13 @@ def get_video():
         if interaction['interaction_id'] == interaction_id:
             transcript = interaction['transcript']
             video_url = interaction['url']
-            
             chapters = generate_chapters_from_transcript(transcript)
-            return render_template('index1.html', chapters=chapters, video_url=video_url)
+            print("final chapters:-",chapters)
+            insights = generate_insights_from_transcript(transcript)
+            print("final insights:-", insights)
+            highlights = generate_highlights(transcript)
+            print("final highlights:-", highlights)
+            return render_template('index.html', chapters=chapters, insights=insights, transcript=transcript, highlights=highlights, video_url=video_url)
 
     return "Video not found for the specified interaction ID", 404
 
@@ -36,11 +40,8 @@ def serve_video(path):
 def index():
     if request.method == 'GET':
         search_query = request.args.get('searchQuery')
-        
+        transcript = request.args.get('transcript')
         if search_query is not None and len(search_query.strip()) != 0:
-            for interaction in video_data['interactions']:
-                if interaction['direction'] == "INB":
-                    transcript = interaction['transcript']
                     search_result = search_api(search_query, transcript)
                     if search_result:
                         return jsonify(search_result) 
--- utils.py
+++ utils.py
@@ -20,6 +20,7 @@ def generate_chapters_from_transcript(transcript):
             {"role": "system", "content": "Break down the entire call into a maximum of 6 chapters (for small discussions of less than 10 minutes, 3 to 4 chapters are enough) based on specific periods and key points discussed."},
             {"role": "system", "content": "include important discussions in chapters e.g at the start when the discussion is starting that time is important to check the topic of discussion. then end discussion is important to check whether the aim of discussion achieved or not and to understand ending remarks. similarly important discussions in between. "},
             {"role": "system", "content": "do not include extra information in response, just provide answer in JSON format everything as string and keep name of json as chapters:{name,offsetStartTime,offsetEndTime}"},
+            {"role": "system", "content": "- time stamps should be accurate and exactly following transcript"},
             {"role": "system", "content": "- Name of the chapter"},
             {"role": "system", "content": "- offsetStartTime of the chapter"},
             {"role": "system", "content": "- offsetEndTime of the chapter"},
@@ -71,8 +72,6 @@ def generate_chapters_from_transcript(transcript):
 
     return []
 
-
-
 def search_api(message, transcript):
     openai_api_key = os.getenv("AZURE_OPENAI_API_KEY")
     
@@ -85,7 +84,7 @@ def search_api(message, transcript):
     prompt = [
         {"role": "system", "content": "Provide a concise answer and context for the following question: 'entered as message'. Search for the answer within the given transcript of a call and summarize it effectively."},
         {"role": "system", "content": "Also, include the start and end times ('offsetStartTime' and 'offsetEndTime') where the answer to the question is discussed. Include only one duration i.e. start and end offset time"},
-        {"role": "system", "content": "Format your response in JSON with the following structure: {'answer': '...', 'offsetStartTime': '...', 'offsetEndTime': '...'}"},
+        {"role": "system", "content": "Format your response in JSON with the following structure: {'answer': '...', 'StartTime': '...', 'EndTime': '...'}"},
         {"role": "system", "content": message},
         {"role": "system", "content": transcript}
     ]
@@ -101,79 +100,135 @@ def search_api(message, transcript):
             presence_penalty=0,
             stop=None
         )
-
-        # Log completion details for debugging
-        # print("Completion:", completion)
-
-        # Extract chapters from completion response
+        
         if completion.choices and len(completion.choices) > 0:
             print(completion.choices,111)
             first_choice = completion.choices[0]
             print(first_choice,222)
             if first_choice.message and hasattr(first_choice.message, 'content'):
-                # print(first_choice.message,111)
                 message_content = first_choice.message.content
-                
-                
-                
-                # return jsonify(message_content)
+            
                 if message_content:
                         try:
-                            # Initialize variables to store extracted data
                             answer = None
-                            offset_start_time = None
-                            offset_end_time = None
-
-                            # Convert message_content to lowercase for case-insensitive search
                             message_content_lower = message_content.lower()
-
-                            # Find indices of target substrings
-                            
                             index1 = message_content_lower.find("answer")
                             print(message_content_lower,index1)
-                            index2 = message_content_lower.find("offsetstarttime")
-                            index3 = message_content_lower.find("offsetendtime")
-
-                            # Extract 'answer' if found
                             if index1 != -1:
                                 start_index = index1 + len("answer") + 1  # Move past the ":"
                                 end_index = message_content.find("\n", start_index)
                                 answer = message_content[start_index:end_index].strip()
-
-                            # Extract 'offsetStartTime' if found
-                            if index2 != -1:
-                                start_index = index2 + len("offsetstarttime") + 1  # Move past the ":"
-                                end_index = message_content.find("\n", start_index)
-                                offset_start_time = message_content[start_index:end_index].strip()
-
-                            # Extract 'offsetEndTime' if found
-                            if index3 != -1:
-                                start_index = index3 + len("offsetendtime") + 1  # Move past the ":"
-                                offset_end_time = message_content[start_index:].strip()
-
-                            # Construct JSON object with extracted data
                             json_data = {
                                 'answer': answer,
-                                'offsetStartTime': offset_start_time,
-                                'offsetEndTime': offset_end_time
                             }
-
                             return json_data
-
                         except Exception as e:
                             print("Error extracting JSON data:", e)
                             return {}
-
-
-                # else:
-                #     print("Empty message content in completion response")
             else:
                 print("Message or content attribute missing in completion choice")
         else:
             print("No valid choices in completion response")
 
     except Exception as e:
-        print("Error generating chapters:", e)
+        print("Error generating response:", e)
 
-    # If no valid chapters are extracted, return an empty list or handle appropriately
+    return []
+
+def generate_highlights(transcript):
+    openai_api_key = os.getenv("AZURE_OPENAI_API_KEY")
+
+    client = AzureOpenAI(
+        api_key=openai_api_key,
+        api_version="2023-12-01-preview",
+        azure_endpoint="https://nice-vip-program.openai.azure.com"
+    )
+    
+    try:        
+        prompt = [
+            {"role": "system", "content": "Extract important highlights from the above transcript. The highlights should capture key moments such as: (1) Reason for the call (2) resolution or explanation provided by agent (3) critical emotions or turning points (e.g., anger, high pitch) (4) final closing remarks but do not include closing greetings and thank you . Do not include identity and demographic details and checks done by the agent. Highlights should not overlap; ensure each highlight has distinct start and end times. Please ensure that the total duration of all selected lines in the highlight should be less than 40% of the total time duration of the transcript. Response Format: Return a JSON object named \"highlights\" containing an array of highlights. Each highlight should include \"content\" for the text of the highlight, \"offsetstarttime\" for the start time of the highlight, and \"offsetendtime\" for the end time of the highlight. The format should be: {\"content\": \"highlight content\", \"offsetstarttime\": \"start time\", \"offsetendtime\": \"end time\"}."},
+            {"role": "system", "content": "Response Format: Return a JSON object named \"highlights\" containing an array of highlights. Each highlight should include \"content\" for the text of the highlight, \"offsetstarttime\" for the start time of the highlight, and \"offsetendtime\" for the end time of the highlight. The format should be: {\"content\": \"highlight content\", \"offsetstarttime\": \"start time\", \"offsetendtime\": \"end time\"}."},
+            {"role": "system", "content": "- content: The text of the highlight."},
+            {"role": "system", "content": "- offsetstarttime: The start time of the highlight."},
+            {"role": "system", "content": "- offsetendtime: The end time of the highlight."},
+            {"role": "system", "content": transcript}
+        ]
+        
+        completion = client.chat.completions.create(
+            model="vip-gpt-35-turbo-16k",
+            messages=prompt,
+            temperature=0.7,
+            max_tokens=800,
+            top_p=0.95,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=None
+        )
+
+        if completion.choices and len(completion.choices) > 0:
+            first_choice = completion.choices[0]
+            if first_choice.message and hasattr(first_choice.message, 'content'):
+                message_content = first_choice.message.content
+                                
+                if message_content:
+                    message_dict = json.loads(message_content)
+                    highlights_json = message_dict.get('highlights')
+                    print(highlights_json)
+                    return highlights_json
+            else:
+                print("Message or content attribute missing in completion choice")
+        else:
+            print("No valid choices in completion response")
+
+    except Exception as e:
+        print("Error generating highlights:", e)
+
+    return []
+
+def generate_insights_from_transcript(transcript):
+    openai_api_key = os.getenv("AZURE_OPENAI_API_KEY")
+    
+    client = AzureOpenAI(
+        api_key=openai_api_key,
+        api_version="2023-12-01-preview",
+        azure_endpoint="https://nice-vip-program.openai.azure.com"
+    )
+    
+    prompt = [
+        {"role": "system", "content": "You are an assistant that provides detailed insights from conversations."},
+        {"role": "user", "content": "Identify key themes, main points, and notable quotes. Summarize context and implications for a comprehensive understanding of the conversation."},
+        {"role": "system", "content": "Identify common contact center industry insights from the given transcripts. These insights include, but are not limited to: 'did the agent greet the customer', 'did the agent ascertain the identity of the customer', 'was the agent polite and empathetic towards the customer', 'was demographic information such as name, location, or contact details verified', 'did the agent resolve the customer's issue', 'was the customer satisfied with the resolution', 'did the agent apologize if there was any inconvenience', 'did the agent follow up or confirm resolution'."},
+        {"role": "user", "content": "Provide the insights along with start and end times in JSON format: {'insights': [{'insight': '...', 'offsetStartTime': '...', 'offsetEndTime': '...'}]}."},
+        {"role": "user", "content": transcript}
+    ]
+
+    try:
+        completion = client.chat.completions.create(
+            model="vip-gpt-35-turbo-16k",
+            messages=prompt,
+            temperature=0.7,
+            max_tokens=800,
+            top_p=0.95,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=None
+        )
+
+        if completion.choices and len(completion.choices) > 0:
+            first_choice = completion.choices[0]
+            if first_choice.message and hasattr(first_choice.message, 'content'):
+                message_content = first_choice.message.content
+                if message_content:
+                    try:
+                        response_data = json.loads(message_content)
+                        if 'insights' in response_data and isinstance(response_data['insights'], list):
+                            print("----",response_data)
+                            insights = response_data['insights']
+                            print("json-----",insights)
+                            return insights
+                    except json.JSONDecodeError as e:
+                        print("Error decoding JSON:", e)
+    except Exception as e:
+        print("Error generating insights:", e)
+    
     return []
